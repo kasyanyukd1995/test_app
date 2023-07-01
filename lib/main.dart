3de4a951417ecf981a01cad56cddb11677d0e5f4
Dzmitry Kasianiuk <dzmitry_kasianiuk@epam.com>
2023-07-01 10:40:54 +0300

    ref

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:http_proxy/http_proxy.dart';
+
 import 'package:test_app/src/dependencies/injection.dart';
 import 'package:test_app/src/presentation/base/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  HttpProxy httpProxy = await HttpProxy.createHttpProxy();
-  httpProxy.host = "192.168.0.176"; // replace with your server ip
-  httpProxy.port = "8082"; // replace with your server port
-  HttpOverrides.global = httpProxy;
+
   configureDependencies();
   runApp(const MyApp());
 }
